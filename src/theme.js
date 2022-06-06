44013596fdc5ed96bd30dddc9b8f9ff300d870af
Amkam <amaduskamara@gmail.com>
2022-06-06 10:15:41 +0000

    Cleared basics buttons usage and create components in components directory

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  pallete: {
+    primary: {
+      main: "#1760a5",
+      light: "skyblue",
+    },
+    secondary: {
+      main: "#15c630",
+    },
+    otherColor: {
+      main: "pink",
+    },
+  },
+});
