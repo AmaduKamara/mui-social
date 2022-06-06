1b9ca0f42847c51e91ec92afaef241ad08f0b3ba
Amkam <amaduskamara@gmail.com>
2022-06-06 09:11:44 +0000

    Add MUI font package and link for Roboto font

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Button } from "@mui/material";
+
 function App() {
   return (
     <div>
       <h1>React Material UI</h1>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </div>
   );
 }
