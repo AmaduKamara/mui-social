40bee5a0410cd901bce4bb5a9c006eca0991d5fe
Amkam <amaduskamara@gmail.com>
2022-06-06 14:03:42 +0000

    Style Navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Sidebar from "./components/Sidebar";
 import Rightbar from "./components/Rightbar";
 import Feed from "./components/Feed";
 import { Box, Stack } from "@mui/material";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <h1>React Material UI</h1>
+      <Navbar />
       <Box>
         <Stack direction="row" spacing={2} justifyContent="space-between">
           <Sidebar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  InputBase,
+  Badge,
+  Avatar,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail";
+import styled from "@emotion/styled";
+import ConnectWithoutContactIcon from "@mui/icons-material/ConnectWithoutContact";
+import { Notifications } from "@mui/icons-material";
+
+const StyledToolBar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "#fff",
+  padding: "2px 10px",
+  borderRadius: "6px",
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "15px",
+  alignItems: "center",
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "8px",
+  alignItems: "center",
+}));
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [open, setOpen] = useState(false);
 
-export default Navbar
+  return (
+    <AppBar position="sticky">
+      <StyledToolBar>
+        <Typography variant="h5" sx={{ display: { xs: "none", sm: "block" } }}>
+          MUI-Scoials
+        </Typography>
+        <ConnectWithoutContactIcon
+          sx={{ display: { xs: "block", sm: "none" } }}
+        />
+        <Search>
+          <InputBase placeholder="Search" />
+        </Search>
+        <Icons sx={{ display: { xs: "none", sm: "flex" } }}>
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://scontent.ffna2-1.fna.fbcdn.net/v/t1.6435-9/129078163_3643902815666949_4927983245120263821_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_ohc=I_BDTJ_sp6wAX8fptbv&_nc_ht=scontent.ffna2-1.fna&oh=00_AT-6ztdHgLGhieQAfpYMkw-lPoj-sWj6hMxyfbWjJp-_gg&oe=62C56FE1"
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+        <UserBox sx={{ display: { xs: "flex", sm: "none" } }}>
+          <Typography variant="span">Amkam</Typography>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://scontent.ffna2-1.fna.fbcdn.net/v/t1.6435-9/129078163_3643902815666949_4927983245120263821_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_ohc=I_BDTJ_sp6wAX8fptbv&_nc_ht=scontent.ffna2-1.fna&oh=00_AT-6ztdHgLGhieQAfpYMkw-lPoj-sWj6hMxyfbWjJp-_gg&oe=62C56FE1"
+            onClick={(e) => setOpen(true)}
+          />
+        </UserBox>
+      </StyledToolBar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Navbar;
